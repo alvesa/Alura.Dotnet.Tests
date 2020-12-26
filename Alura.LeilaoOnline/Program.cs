b624657a67466a05e7f6a63d0ac636ce7232b79f
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2020-12-26 07:00:03 -0300

    Adicionando metodo de testes e mudando cor do resultado

--- Alura.LeilaoOnline/Program.cs
+++ Alura.LeilaoOnline/Program.cs
@@ -7,6 +7,11 @@ namespace Alura.LeilaoOnline
     {
         static void Main(string[] args)
         {
+            LeilaoComVariosLances();
+            LeilaoComUmLance();
+        }
+        private static void LeilaoComVariosLances()
+        {
             // Arrange
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
@@ -21,7 +26,45 @@ namespace Alura.LeilaoOnline
             leilao.TerminaPregao();
 
             // Assert
-            Console.WriteLine(leilao.Ganhador.Valor);
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            
+            Verifica(valorEsperado, valorObtido);
+
+        }
+
+        static void LeilaoComUmLance()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+            
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+
+            if(esperado == obtido){
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else{
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"TESTE FALHOU! Esperado: {esperado}, obtido: {obtido}");
+            }
+
+            Console.ForegroundColor = cor;
         }
     }
 }

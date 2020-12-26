1bf5fb054270ebd3266a2c7945653c38d8b3733c
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2020-12-26 07:54:40 -0300

    Resumindo os testes iguais em apenas 1 teste

--- Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -6,36 +6,6 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTestes
     {
         [Fact]
-        public void LeilaoComTresClientes()
-        {
-            // Arrange
-            // Dado leilao com pelo menos 1 lance realizado
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-            var beltrano = new Interessada("Beltrano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(beltrano, 1400);
-
-            // Act
-            // Quando o leilao/pregao termina
-            leilao.TerminaPregao();
-
-            // Assert
-            // O valor esperado é o maior valor dado pelo
-            // E o cliente ganhador é o que deu o maior lance
-            var valorEsperado = 1400;
-            var valorObtido = leilao.Ganhador.Valor;
-            
-            Assert.Equal(valorEsperado, valorObtido);
-            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
-        }
-
-        [Fact]
         public void LeilaoSemLances()
         {
             // Arrange
@@ -51,66 +21,26 @@ namespace Alura.LeilaoOnline.Tests
             Assert.Equal(valorEsperado, valorObtido);
         }
 
-        [Fact]
-        public void LeilaoComLancesOrdenadosPorValor()
-        {
-            // Arrange
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
-            // Act
-            leilao.TerminaPregao();
-
-            // Assert
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-            
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComVariosLances()
-        {
-            // Arrange
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
-            // Act
-            leilao.TerminaPregao();
-
-            // Assert
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-            
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComUmLance()
+        [Theory]
+        [InlineData(1000, new double[] {800, 900, 1000, 1200})]
+        [InlineData(1000, new double[] {800, 900, 1000, 990})]
+        [InlineData(800, new double[] {800})]
+        public void LeilaoComVariosLances(double valorEsperado, double[] ofertas )
         {
             // Arrange
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
 
-            leilao.RecebeLance(fulano, 800);
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
 
             // Act
             leilao.TerminaPregao();
 
             // Assert
-            var valorEsperado = 800;
             var valorObtido = leilao.Ganhador.Valor;
             
             Assert.Equal(valorEsperado, valorObtido);
--- Alura.LeilaoOnline/Program.cs
+++ Alura.LeilaoOnline/Program.cs
@@ -7,21 +7,5 @@ namespace Alura.LeilaoOnline
         static void Main(string[] args)
         {
         }
-
-        private static void Verifica(double esperado, double obtido)
-        {
-            var cor = Console.ForegroundColor;
-
-            if(esperado == obtido){
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("TESTE OK");
-            }
-            else{
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"TESTE FALHOU! Esperado: {esperado}, obtido: {obtido}");
-            }
-
-            Console.ForegroundColor = cor;
-        }
     }
 }

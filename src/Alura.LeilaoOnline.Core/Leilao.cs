c4146c30f198b538a944fd1e344e6596b6d3776d
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2020-12-28 07:37:04 -0300

    Criando metodo privado para validar o lance

--- src/Alura.LeilaoOnline.Core/Leilao.cs
+++ src/Alura.LeilaoOnline.Core/Leilao.cs
@@ -25,14 +25,17 @@ namespace Alura.LeilaoOnline.Core
             Estado = EstadoLeilao.LeilaoAntesDoPregao;
         }
 
+        private bool LanceAceito(Interessada cliente, double valor)
+        {
+            return (Estado == EstadoLeilao.LeilaoEmAndamento) && (cliente != _ultimoCliente);
+        }
+
         public void RecebeLance(Interessada cliente, double valor)
         {
-            if(Estado == EstadoLeilao.LeilaoEmAndamento)
+            if(LanceAceito(cliente, valor))
             {
-                if(cliente != _ultimoCliente){
-                    _lances.Add(new Lance(cliente, valor));
-                    _ultimoCliente = cliente;
-                }
+                _lances.Add(new Lance(cliente, valor));
+                _ultimoCliente = cliente;
             }
         }
 
